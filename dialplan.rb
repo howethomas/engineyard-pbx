c2df2001855f0343227bd94c6f8d63fdcc588bd5
Jay Phillips <jicksta+git@gmail.com>
2008-01-14 17:39:12 -0800

    Dialplan is just about what I think they need, sans the harder queue stuff

--- dialplan.rb
+++ dialplan.rb
@@ -1,17 +1,42 @@
 ahn {
   
-  choice = input 4, :timeout => 5.seconds, :play => 'engineyard/prompt'
+  @choice = input 4, :timeout => 4.seconds, :play => 'engineyard/prompt'
+  puts "choice: #{@choice.inspect}"
   
-  if (1..3).include? choice.to_i
-    play 'hello-world'
-  elsif employee = Employee.find_by_extension(choice)
-    mobile_number = employee.mobile_number
-    if mobile_number
-      dial "SIP/trunk/#{mobile_number}", :callerid => callerid
-    else
-      # Say: Sorry, the person you're calling cannot be reached.
-    end
-  elsif (1_000...10_000).include? choice.to_i
-    join choice
+  case @choice.to_i
+    when 200...300: +employees
+    when 1      
+      # IMPLEMENT GROUP DIALING
+      # queue :sales
+    when 2 
+      # tech support
+    when 3
+      # finance
+    when 0
+      # operator
+  end
+}
+
+employees {
+  puts "Dialing an employee!"
+  
+  employee = Employee.find_by_extension @choice
+  mobile_number = employee.mobile_number if employee
+  
+  if mobile_number
+    #dial "SIP/voipms/#{mobile_number}", :callerid => callerid
+    puts "DIALING #{mobile_number}! mocked out"
+  else
+    play 'invalid'
+    +ahn
+  end
+
+}
+
+internal {
+  # Only allow internal users to dial into conferences
+  case extension
+    when 200...300
+    when 1_000...10_000
   end
 }
