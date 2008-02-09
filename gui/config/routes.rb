34c810cf61217972b9d8bc9f2f12725c70a7b341
Jay Phillips <jicksta+git@gmail.com>
2008-02-08 16:43:06 -0800

    Created an extension manager which will ultimately allow the IVR tree to be modified

--- gui/config/routes.rb
+++ gui/config/routes.rb
@@ -1,6 +1,7 @@
 ActionController::Routing::Routes.draw do |map|
   
   map.group_editor 'groups/editor', :controller => "groups", :action => "editor"
+  map.extension_manager 'extension_manager', :controller => "groups", :action => "extension_manager"
 
   # map.resources :groups
 
@@ -33,7 +34,6 @@ ActionController::Routing::Routes.draw do |map|
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
   map.root :controller => "welcome"
-
 
   # See how all your routes lay out with "rake routes"
 
